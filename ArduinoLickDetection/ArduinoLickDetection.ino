e4414db5444f24d829d091131640b5fa8083e84d
Maxime Verstraeten <v.maxime@gmail.com>
2019-08-08 15:13:31 +0100

    Add files via upload

---
+++ ArduinoLickDetection/ArduinoLickDetection.ino
@@ -0,0 +1,41 @@
+//Functions declaration
+void TTLinputRising();
+
+uint32_t pressedTimeStamp;
+const uint32_t debounceDelay = 500;
+
+volatile boolean flagTTLinput = false;
+
+
+const int TTLinputPin = 2;
+const int outputPin = 6;
+
+
+void setup() {
+  pressedTimeStamp = millis();
+  Serial.begin(9600);
+  pinMode(TTLinputPin, INPUT);
+  pinMode(outputPin, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(TTLinputPin), TTLinputRising, RISING);
+}
+
+void loop(){
+  if (flagTTLinput) {
+    flagTTLinput = false;
+    valveActivation();
+  }
+}
+
+
+void TTLinputRising() {
+  flagTTLinput = true;
+}
+
+void valveActivation() {
+  if (millis() > pressedTimeStamp + debounceDelay) {
+    pressedTimeStamp = millis();
+    digitalWrite(outputPin, !digitalRead(outputPin));
+    Serial.println("valve activated");
+  }
+}
+
