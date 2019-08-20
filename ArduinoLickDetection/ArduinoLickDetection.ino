ab10fdfd35d236f2ee9431ca91d5353c3a011175
v.maxime@gmail.com <v.maxime@gmail.com>
2019-08-20 17:25:37 +0100

    simple lick v1

--- ArduinoLickDetection/ArduinoLickDetection.ino
+++ ArduinoLickDetection/ArduinoLickDetection.ino
@@ -1,28 +1,64 @@
+//https://github.com/GreyGnome/EnableInterrupt
+#include "EnableInterrupt/EnableInterrupt.h"
+
+
 //Functions declaration
 void TTLinputRising();
 
 uint32_t pressedTimeStamp;
-const uint32_t debounceDelay = 500;
+const uint32_t debounceDelay = 110;
 
 volatile boolean flagTTLinput = false;
-
+volatile boolean flagSwitchInput = false;
+volatile boolean flagBNCTrigger = false;
+volatile boolean flagPushButton = false;
 
 const int TTLinputPin = 2;
 const int outputPin = 6;
+const int switchPin = A0;
+const int BNCTriggerPin = 3;
+const int pushButtonPin = 13;
 
 
 void setup() {
   pressedTimeStamp = millis();
-  Serial.begin(9600);
+  Serial.begin(500000);
   pinMode(TTLinputPin, INPUT);
   pinMode(outputPin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(TTLinputPin), TTLinputRising, RISING);
+  //pinMode(switchPin, INPUT);
+  //pinMode(BNCTriggerPin, INPUT);
+  pinMode(pushButtonPin, INPUT_PULLUP);
+
+  enableInterrupt(TTLinputPin, TTLinputRising, RISING);
+  //enableInterrupt(switchPin, switchInput, CHANGE);
+  //enableInterrupt(BNCTriggerPin, BNCTrigger, RISING);
+  enableInterrupt(pushButtonPin, pushButton, RISING);
 }
 
-void loop(){
+void loop() {
+  /*
+  digitalWrite(outputPin, HIGH);
+  Serial.println("on");
+  delay(100);
+  digitalWrite(outputPin,LOW);
+  Serial.println("off");
+  delay(50);*/
+  
   if (flagTTLinput) {
     flagTTLinput = false;
-    valveActivation();
+    valveInputActivation();
+  }
+  if (flagSwitchInput) {
+    flagSwitchInput = false;
+    valveSwitchActivation();
+  }
+  if (flagBNCTrigger) {
+    flagBNCTrigger = false;
+    valveBNCTriggerActivation();
+  }
+  if(flagPushButton){
+    flagPushButton = false;
+    valvePushButtonActivation();
   }
 }
 
@@ -31,11 +67,65 @@ void TTLinputRising() {
   flagTTLinput = true;
 }
 
-void valveActivation() {
+void switchInput() {
+  flagSwitchInput = true;
+}
+
+void BNCTrigger() {
+  flagBNCTrigger = true;
+}
+
+void pushButton(){
+  flagPushButton = true;
+}
+
+void valveInputActivation() {
+  if (millis() > pressedTimeStamp + debounceDelay) {
+    delay(1);
+    if (digitalRead(TTLinputPin)) { //debounce on release
+      pressedTimeStamp = millis();
+      digitalWrite(outputPin, HIGH);
+      delay(100);
+      digitalWrite(outputPin, LOW);
+      //digitalWrite(outputPin, digitalRead(switchPin));
+      Serial.println("valve activated input");
+    }
+  }
+}
+
+void valveSwitchActivation() {
   if (millis() > pressedTimeStamp + debounceDelay) {
     pressedTimeStamp = millis();
-    digitalWrite(outputPin, !digitalRead(outputPin));
-    Serial.println("valve activated");
+    digitalWrite(outputPin, digitalRead(switchPin));
+    Serial.println("valve activated switch");
+  }
+}
+
+void valveBNCTriggerActivation() {
+  if (millis() > pressedTimeStamp + debounceDelay) {
+    delay(1);
+    if (digitalRead(BNCTriggerPin)) { //debounce on release
+      pressedTimeStamp = millis();
+      digitalWrite(outputPin, HIGH);
+      delay(500);
+      digitalWrite(outputPin, LOW);
+      //digitalWrite(outputPin, digitalRead(switchPin));
+      Serial.println("valve activated BNC");
+    }
+  }
+}
+
+void valvePushButtonActivation(){
+    if (millis() > pressedTimeStamp + debounceDelay) {
+    delay(1);
+    if (!digitalRead(pushButtonPin)) { //debounce on release
+      pressedTimeStamp = millis();
+      digitalWrite(outputPin, HIGH);
+      delay(200);
+      digitalWrite(outputPin, LOW);
+      //digitalWrite(outputPin, digitalRead(switchPin));
+      Serial.println("valve activated pushButton");
+    }
   }
 }
 
