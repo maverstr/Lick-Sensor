ba0398a910eb3e025f2ed876a0bfe6799959e00e
v.maxime@gmail.com <v.maxime@gmail.com>
2019-09-18 16:37:43 +0100

    added auto drain and new workflow

--- ArduinoLickDetection/ArduinoLickDetection.ino
+++ ArduinoLickDetection/ArduinoLickDetection.ino
@@ -1,98 +1,142 @@
 //https://github.com/GreyGnome/EnableInterrupt
 #include "EnableInterrupt/EnableInterrupt.h"
 
-
-//Functions declaration
-void TTLinputRising();
-
 uint32_t pressedTimeStamp;
+uint32_t pressedTimeStampSensor;
+uint32_t pressedTimeStampBNC;
+uint32_t timeStampDropletPresent;
+
 const uint32_t debounceDelay = 300;
 
-volatile boolean flagTTLinput = false;
-volatile boolean flagSwitchInput = false;
+//INTERRUPT FLAGS
+volatile boolean flagSensorInput = false;
+volatile boolean flagswitchInputInterrupt = false;
 volatile boolean flagBNCTrigger = false;
 volatile boolean flagPushButton = false;
 volatile boolean flagAirValveInput = false;
 
-const int TTLinputPin = 2;
+//GPIO
+const int sensorInputPin = 2;
 const int valveOutputPin = 6;
-const int airValveOutputPin = 7;
+const int drainValveOutputPin = 7;
 const int switchPin = A0;
 const int BNCTriggerPin = 3;
 const int pushButtonPin = 13;
 
+//DURATIONS
+const int mainValveActivationDuration = 200;
+const int drainValveActivationDuration = 500;
+const int drainValveAutoActivationDelay = 6000;
+
+//ODORS
+const int rewardList[] = {1}; //TO ADAPT TO THE EXPERIMENT
+bool flagRewardingOdor = false;
+int odorCounter = 0;
+int rewardListLength = 0;
+
+//DROPLET FLAG
+bool isDropletPresent = false;
+
 
 void setup() {
+  rewardListLength = sizeof(rewardList);
   pressedTimeStamp = millis();
   Serial.begin(500000);
-  pinMode(TTLinputPin, INPUT);
+  pinMode(sensorInputPin, INPUT);
   pinMode(valveOutputPin, OUTPUT);
+  pinMode(drainValveOutputPin, OUTPUT);
   pinMode(switchPin, INPUT);
   pinMode(BNCTriggerPin, INPUT);
   pinMode(pushButtonPin, INPUT_PULLUP);
 
-  enableInterrupt(TTLinputPin, TTLinputRising, RISING);
-  enableInterrupt(switchPin, switchInput, CHANGE);
-  enableInterrupt(BNCTriggerPin, BNCTrigger, RISING);
-  enableInterrupt(pushButtonPin, pushButton, RISING);
-  //enableInterrput( ..., AirValveTrigger, RISING);
+  enableInterrupt(sensorInputPin, sensorInputInterrrupt, RISING);
+  enableInterrupt(pushButtonPin, AirValveTriggerInterrupt, RISING); //to trigger the drain
+  enableInterrupt(switchPin, switchInputInterrupt, CHANGE);
+  //enableInterrupt(pushButtonPin, pushButtonInterrupt, RISING);//to trigger the main valve
+  enableInterrupt(BNCTriggerPin, BNCTriggerInterrupt, RISING);
 }
 
 void loop() {
-  if (flagTTLinput) {
-    flagTTLinput = false;
-    valveInputActivation();
+  if (flagSensorInput) {
+    flagSensorInput = false;
+    //valveSensorActivation();
+    rewardDelivery();
   }
-  if (flagSwitchInput) {
-    flagSwitchInput = false;
+  if (flagswitchInputInterrupt) {
+    flagswitchInputInterrupt = false;
     valveSwitchActivation();
-  }
-  if (flagBNCTrigger) {
-    flagBNCTrigger = false;
-    valveBNCTriggerActivation();
   }
   if (flagPushButton) {
     flagPushButton = false;
     valvePushButtonActivation();
   }
-  if(flagAirValveInput){
+  if (flagAirValveInput) {
     flagAirValveInput = false;
-    airValveActivation();
+    drainValveActivation();
   }
+  if (flagBNCTrigger) {
+    flagBNCTrigger = false;
+    assessRewardingOdorList();
+  }
+  dropletAutomaticRemoval();
 }
 
+// ===============================
+// =====      INTERRUPTS      ====
+// ===============================
 
-void TTLinputRising() {
-  flagTTLinput = true;
+void sensorInputInterrrupt() {
+  flagSensorInput = true;
 }
 
-void switchInput() {
-  flagSwitchInput = true;
+void switchInputInterrupt() {
+  flagswitchInputInterrupt = true;
 }
 
-void BNCTrigger() {
-  flagBNCTrigger = true;
-}
-
-void pushButton() {
+void pushButtonInterrupt() {
   flagPushButton = true;
 }
 
-void airValveTrigger(){
+void AirValveTriggerInterrupt() {
   flagAirValveInput = true;
 }
 
+void BNCTriggerInterrupt() {
+  flagBNCTrigger = true;
+}
 
-void valveInputActivation() {
-  if (millis() > pressedTimeStamp + debounceDelay) {
+// ===============================
+// ====  Activation handling  ====
+// ===============================
+
+void rewardDelivery() {
+  if (millis() > pressedTimeStamp + debounceDelay && flagRewardingOdor) {
     delay(1);
-    if (digitalRead(TTLinputPin)) { //debounce on release
+    if (digitalRead(sensorInputPin)) { //debounce on release
       pressedTimeStamp = millis();
       digitalWrite(valveOutputPin, HIGH);
-      delay(100);
+      delay(mainValveActivationDuration);
       //digitalWrite(valveOutputPin, LOW);
       digitalWrite(valveOutputPin, digitalRead(switchPin));
       Serial.println("valve activated input");
+      isDropletPresent = true;
+      timeStampDropletPresent = millis();
+    }
+  }
+}
+
+void valveSensorActivation() {
+  if (millis() > pressedTimeStampSensor + debounceDelay) {
+    delay(1);
+    if (digitalRead(sensorInputPin)) { //debounce on release
+      pressedTimeStampSensor = millis();
+      digitalWrite(valveOutputPin, HIGH);
+      delay(mainValveActivationDuration);
+      //digitalWrite(valveOutputPin, LOW);
+      digitalWrite(valveOutputPin, digitalRead(switchPin));
+      Serial.println("valve activated input");
+      timeStampDropletPresent = millis();
+      isDropletPresent = true;
     }
   }
 }
@@ -102,19 +146,9 @@ void valveSwitchActivation() {
     pressedTimeStamp = millis();
     digitalWrite(valveOutputPin, digitalRead(switchPin));
     Serial.println("valve activated switch");
-  }
-}
-
-void valveBNCTriggerActivation() {
-  if (millis() > pressedTimeStamp + debounceDelay) {
-    delay(1);
-    if (digitalRead(BNCTriggerPin)) { //debounce on release
-      pressedTimeStamp = millis();
-      //digitalWrite(valveOutputPin, HIGH);
-      //delay(500);
-      //digitalWrite(valveOutputPin, LOW);
-      //digitalWrite(valveOutputPin, digitalRead(switchPin));
-      Serial.println("valve activated BNC");
+    if (!digitalRead(switchPin)) { //if switched off, meaning stopped flowing
+    timeStampDropletPresent = millis();
+      isDropletPresent = true;
     }
   }
 }
@@ -125,22 +159,54 @@ void valvePushButtonActivation() {
     if (!digitalRead(pushButtonPin)) { //debounce on release
       pressedTimeStamp = millis();
       digitalWrite(valveOutputPin, HIGH);
-      delay(200);
+      delay(mainValveActivationDuration);
       //digitalWrite(valveOutputPin, LOW);
       digitalWrite(valveOutputPin, digitalRead(switchPin));
       Serial.println("valve activated pushButton");
+      isDropletPresent = true;
+      timeStampDropletPresent = millis();
     }
   }
 }
 
-void airValveActivation() {
+void drainValveActivation() {
   if (millis() > pressedTimeStamp + debounceDelay) {
     pressedTimeStamp = millis();
-    digitalWrite(airValveOutputPin, HIGH);
-    delay(200);
-    digitalWrite(airValveOutputPin, LOW);
-    Serial.println("AIRVALVE activated");
-
+    digitalWrite(drainValveOutputPin, HIGH);
+    delay(drainValveActivationDuration);
+    digitalWrite(drainValveOutputPin, LOW);
+    Serial.println("DRAIN activated");
+    isDropletPresent = false;
   }
 }
+
+// ===============================
+// =====     odors reward     ====
+// ===============================
+
+void assessRewardingOdorList() {
+  if (millis() > pressedTimeStampBNC + debounceDelay) {
+    pressedTimeStampBNC = millis();
+    if (odorCounter > rewardListLength) {
+      odorCounter = 0;
+    }
+    flagRewardingOdor = rewardList[odorCounter];
+    odorCounter++;
+  }
+}
+
+// ===============================
+// ===== droplet auto removal ====
+// ===============================
+
+void dropletAutomaticRemoval() {
+  if (isDropletPresent && millis() > timeStampDropletPresent + drainValveAutoActivationDelay) {
+    digitalWrite(drainValveOutputPin, HIGH);
+    delay(drainValveActivationDuration);
+    digitalWrite(drainValveOutputPin, LOW);
+    Serial.print("DRAIN AUTO ACTIVATION AFTER : "); Serial.println(drainValveAutoActivationDelay);
+    isDropletPresent = false;
+  }
+}
+
 
