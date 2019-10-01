dbd61934269ba9f6476cb348d5570551df70bd0d
v.maxime@gmail.com <v.maxime@gmail.com>
2019-10-01 11:06:53 +0100

    renamed multiple variables + added hardware interface for drain valve

--- ArduinoLickDetection/ArduinoLickDetection.ino
+++ ArduinoLickDetection/ArduinoLickDetection.ino
@@ -1,7 +1,8 @@
 //https://github.com/GreyGnome/EnableInterrupt
 #include "EnableInterrupt/EnableInterrupt.h"
 
-uint32_t pressedTimeStamp;
+uint32_t pressedTimeStampMainValve;
+uint32_t pressedTimeStampDrainValve;
 uint32_t pressedTimeStampSensor;
 uint32_t pressedTimeStampBNC;
 uint32_t timeStampDropletPresent;
@@ -9,19 +10,29 @@ uint32_t timeStampDropletPresent;
 const uint32_t debounceDelay = 500;
 
 //INTERRUPT FLAGS
-volatile boolean flagSensorInput = false;
-volatile boolean flagswitchInputInterrupt = false;
+volatile boolean flagTouchSensorInput = false;
+volatile boolean flagMainValvePushButton = false;
+volatile boolean flagDrainValvePushButton = false;
+volatile boolean flagMainValveSwitchInput = false;
+volatile boolean flagDrainValveSwitchInput = false;
 volatile boolean flagBNCTrigger = false;
-volatile boolean flagPushButton = false;
-volatile boolean flagAirValveInput = false;
 
-//GPIO
-const int sensorInputPin = 2;
-const int valveOutputPin = 6;
+// ===============================
+// =====         GPIO         ====
+// ===============================
+//valves
+const int mainValveOutputPin = 6;
 const int drainValveOutputPin = 7;
-const int switchPin = A0;
+//toggle switches
+const int mainValveSwitchPin = A0;
+const int drainValveSwitchPin = A1;
+//pushbuttons
+const int mainValvePushButtonPin = 13;
+const int drainValvePushButtonPin = 12;
+//BNC
 const int BNCTriggerPin = 3;
-const int pushButtonPin = 13;
+//touchSensor
+const int touchSensorInputPin = 2;
 
 //DURATIONS
 const int mainValveActivationDuration = 200;
@@ -40,39 +51,54 @@ bool isDropletPresent = false;
 
 void setup() {
   rewardListLength = sizeof(rewardList);
-  pressedTimeStamp = millis();
-  Serial.begin(500000);
-  pinMode(sensorInputPin, INPUT);
-  pinMode(valveOutputPin, OUTPUT);
-  pinMode(drainValveOutputPin, OUTPUT);
-  pinMode(switchPin, INPUT);
-  pinMode(BNCTriggerPin, INPUT);
-  pinMode(pushButtonPin, INPUT_PULLUP);
+  //sets initial time stamps
+  pressedTimeStampMainValve = millis();
+  pressedTimeStampDrainValve = millis();
+  pressedTimeStampSensor = millis();
+  pressedTimeStampBNC = millis();
 
-  enableInterrupt(sensorInputPin, sensorInputInterrrupt, FALLING);
-  enableInterrupt(pushButtonPin, AirValveTriggerInterrupt, RISING); //to trigger the drain
-  enableInterrupt(switchPin, switchInputInterrupt, CHANGE);
-  //enableInterrupt(pushButtonPin, pushButtonInterrupt, RISING);//to trigger the main valve
-  //enableInterrupt(BNCTriggerPin, BNCTriggerInterrupt, RISING);
+  Serial.begin(500000);
+  pinMode(mainValveOutputPin, OUTPUT);
+  pinMode(drainValveOutputPin, OUTPUT);
+  pinMode(mainValveSwitchPin, INPUT_PULLUP);
+  pinMode(drainValveSwitchPin, INPUT_PULLUP);
+  pinMode(mainValvePushButtonPin, INPUT_PULLUP);
+  pinMode(drainValvePushButtonPin, INPUT_PULLUP);
+  pinMode(BNCTriggerPin, INPUT);
+  pinMode(touchSensorInputPin, INPUT);
+
+  //Sets the interrupts (pinNumber, ISR(interrupt service routine), edge of signal detection)
+  enableInterrupt(touchSensorInputPin, touchSensorInputInterrrupt, FALLING);
+  enableInterrupt(mainValvePushButtonPin, mainValvePushButtonInterrupt, RISING);
+  enableInterrupt(mainValveSwitchPin, mainValveSwitchInterrupt, CHANGE);
+  enableInterrupt(drainValvePushButtonPin, drainValvePushButtonInterrupt, RISING);
+  enableInterrupt(drainValveSwitchPin, drainValveSwitchInterrupt, CHANGE);
+  enableInterrupt(BNCTriggerPin, BNCTriggerInterrupt, RISING);
 }
 
 void loop() {
-  if (flagSensorInput) {
-    flagSensorInput = false;
-    valveSensorActivation();
+  //The loop assess all the flags from the interrupts and activates the relevant functions.
+  //It then assess the time since a droplet is present for automatic droplet removal.
+  if (flagTouchSensorInput) {
+    flagTouchSensorInput = false;
+    mainValveTouchSensorActivation();
     //rewardDelivery();
   }
-  if (flagswitchInputInterrupt) {
-    flagswitchInputInterrupt = false;
-    valveSwitchActivation();
+  if (flagMainValvePushButton) {
+    flagMainValvePushButton = false;
+    mainValvePushButtonActivation();
   }
-  if (flagPushButton) {
-    flagPushButton = false;
-    valvePushButtonActivation();
+  if (flagDrainValvePushButton) {
+    flagDrainValvePushButton = false;
+    drainValvePushButtonActivation();
   }
-  if (flagAirValveInput) {
-    flagAirValveInput = false;
-    drainValveActivation();
+  if (flagMainValveSwitchInput) {
+    flagMainValveSwitchInput = false;
+    mainValveSwitchActivation();
+  }
+  if (flagDrainValveSwitchInput) {
+    flagDrainValveSwitchInput = false;
+    drainValveSwitchActivation();
   }
   if (flagBNCTrigger) {
     flagBNCTrigger = false;
@@ -85,20 +111,24 @@ void loop() {
 // =====      INTERRUPTS      ====
 // ===============================
 
-void sensorInputInterrrupt() {
-  flagSensorInput = true;
+void touchSensorInputInterrrupt() {
+  flagTouchSensorInput = true;
 }
 
-void switchInputInterrupt() {
-  flagswitchInputInterrupt = true;
+void mainValvePushButtonInterrupt() {
+  flagMainValvePushButton = true;
 }
 
-void pushButtonInterrupt() {
-  flagPushButton = true;
+void drainValvePushButtonInterrupt() {
+  flagDrainValvePushButton = true;
 }
 
-void AirValveTriggerInterrupt() {
-  flagAirValveInput = true;
+void mainValveSwitchInterrupt() {
+  flagMainValveSwitchInput = true;
+}
+
+void drainValveSwitchInterrupt() {
+  flagDrainValveSwitchInput = true;
 }
 
 void BNCTriggerInterrupt() {
@@ -110,73 +140,86 @@ void BNCTriggerInterrupt() {
 // ===============================
 
 void rewardDelivery() {
-  if (millis() > pressedTimeStamp + debounceDelay && flagRewardingOdor) {
+  if (millis() > pressedTimeStampSensor + debounceDelay && flagRewardingOdor) {
     delay(1);
-    if (!digitalRead(sensorInputPin)) { //debounce on release
-      pressedTimeStamp = millis();
-      digitalWrite(valveOutputPin, HIGH);
+    if (!digitalRead(touchSensorInputPin)) { //debounce on release
+      pressedTimeStampSensor = millis();
+      digitalWrite(mainValveOutputPin, HIGH);
       delay(mainValveActivationDuration);
-      //digitalWrite(valveOutputPin, LOW);
-      digitalWrite(valveOutputPin, digitalRead(switchPin));
-      Serial.println("valve activated input");
+      //digitalWrite(mainValveOutputPin, LOW);
+      digitalWrite(mainValveOutputPin, digitalRead(mainValveSwitchPin));
+      Serial.println("MAIN activated input");
       isDropletPresent = true;
       timeStampDropletPresent = millis();
     }
   }
 }
 
-void valveSensorActivation() {
+void mainValveTouchSensorActivation() {
   if (millis() > pressedTimeStampSensor + debounceDelay) {
     delay(1);
-    if (!digitalRead(sensorInputPin)) { //debounce on release
+    if (!digitalRead(touchSensorInputPin)) { //debounce on release
       pressedTimeStampSensor = millis();
-      digitalWrite(valveOutputPin, HIGH);
+      digitalWrite(mainValveOutputPin, HIGH);
       delay(mainValveActivationDuration);
-      //digitalWrite(valveOutputPin, LOW);
-      digitalWrite(valveOutputPin, digitalRead(switchPin));
-      Serial.println("valve activated input");
+      //digitalWrite(mainValveOutputPin, LOW);
+      digitalWrite(mainValveOutputPin, digitalRead(mainValveSwitchPin));
+      Serial.println("MAIN activated input");
       timeStampDropletPresent = millis();
       isDropletPresent = true;
     }
   }
 }
 
-void valveSwitchActivation() {
-  if (millis() > pressedTimeStamp + debounceDelay) {
-    pressedTimeStamp = millis();
-    digitalWrite(valveOutputPin, digitalRead(switchPin));
-    Serial.println("valve activated switch");
-    if (!digitalRead(switchPin)) { //if switched off, meaning stopped flowing
-    timeStampDropletPresent = millis();
-      isDropletPresent = true;
-    }
-  }
-}
-
-void valvePushButtonActivation() {
-  if (millis() > pressedTimeStamp + debounceDelay) {
-    delay(1);
-    if (!digitalRead(pushButtonPin)) { //debounce on release
-      pressedTimeStamp = millis();
-      digitalWrite(valveOutputPin, HIGH);
-      delay(mainValveActivationDuration);
-      //digitalWrite(valveOutputPin, LOW);
-      digitalWrite(valveOutputPin, digitalRead(switchPin));
-      Serial.println("valve activated pushButton");
-      isDropletPresent = true;
+void mainValveSwitchActivation() {
+  if (millis() > pressedTimeStampMainValve + debounceDelay) {
+    pressedTimeStampMainValve = millis();
+    digitalWrite(mainValveOutputPin, digitalRead(mainValveSwitchPin));
+    Serial.println("MAIN activated switch");
+    if (!digitalRead(mainValveSwitchPin)) { //if switched off, meaning stopped flowing
       timeStampDropletPresent = millis();
+      isDropletPresent = true;
     }
   }
 }
 
-void drainValveActivation() {
-  if (millis() > pressedTimeStamp + debounceDelay) {
-    pressedTimeStamp = millis();
-    digitalWrite(drainValveOutputPin, HIGH);
-    delay(drainValveActivationDuration);
-    digitalWrite(drainValveOutputPin, LOW);
-    Serial.println("DRAIN activated");
+void drainValveSwitchActivation() {
+  if (millis() > pressedTimeStampDrainValve + debounceDelay) {
+    pressedTimeStampDrainValve = millis();
+    digitalWrite(drainValveOutputPin, digitalRead(drainValveSwitchPin));
+    Serial.println("DRAIN activated switch");
     isDropletPresent = false;
+  }
+}
+
+void mainValvePushButtonActivation() {
+  if (millis() > pressedTimeStampMainValve + debounceDelay) {
+    delay(1);
+    if (!digitalRead(mainValvePushButtonPin)) { //debounce on release
+      pressedTimeStampMainValve = millis();
+      digitalWrite(mainValveOutputPin, HIGH);
+      delay(mainValveActivationDuration);
+      //digitalWrite(mainValveOutputPin, LOW);
+      digitalWrite(mainValveOutputPin, digitalRead(mainValveSwitchPin));
+      Serial.println("MAIN activated pushButton");
+      isDropletPresent = true;
+      timeStampDropletPresent = millis();
+    }
+  }
+}
+
+void drainValvePushButtonActivation() {
+  if (millis() > pressedTimeStampDrainValve + debounceDelay) {
+    delay(1);
+    if (!digitalRead(drainValvePushButtonPin)) { //debounce on release
+      pressedTimeStampDrainValve = millis();
+      digitalWrite(drainValveOutputPin, HIGH);
+      delay(drainValveActivationDuration);
+      //digitalWrite(drainValveOutputPin, LOW);
+      digitalWrite(drainValveOutputPin, digitalRead(drainValveSwitchPin));
+      Serial.println("DRAIN activated pushButton");
+      isDropletPresent = false;
+    }
   }
 }
 
